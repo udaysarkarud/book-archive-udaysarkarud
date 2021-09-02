53a8c7781eab0eae5a4c529f7169dd99b2bb2c23
udaysarkarud <udaysarkarud@gmail.com>
2021-09-02 18:09:17 +0600

    checked and update

--- js/app.js
+++ js/app.js
@@ -1,94 +1,105 @@
 const searchInput = document.getElementById('searchInput');
-const bookSearchArea = document.getElementById('bookSearchArea');
+const erroTypeAName = document.getElementById('erroTypeAName');
+const lodingData = document.getElementById('lodingData');
 const totalBooksFound = document.getElementById('totalBooksFound')
-const erroTypeAName = document.getElementById('erroTypeAName')
-const erroNoResults = document.getElementById('erroNoResults')
+const bookSearchArea = document.getElementById('bookSearchArea');
+const erroNoResults = document.getElementById('erroNoResults');
 
+//Button Click Function
 document.getElementById('searchBtn').addEventListener('click', () => {
     if (searchInput.value !== '') {
         getData(searchInput.value);
         searchInput.value = '';
 
-        //Error Handel Section
-        errorHandel('hide', 'hide', 'hide', 'hide')
+        //loading and Error function
+        loadingAndError('show', 'hide', 'hide', 'hide');
     }
     else {
-        //Error Handel Section       
-        errorHandel('show', 'hide', 'hide', 'hide')
+        //loading and Error function    
+        loadingAndError('hide', 'show', 'hide', 'hide');
     }
 })
 
+//connect with API
 const connectApi = async (query) => {
-    const res = await fetch(`http://openlibrary.org/search.json?q=${query}`)
+    const res = await fetch(`https://openlibrary.org/search.json?q=${query}`);
     const data = await res.json();
-    return data
+    return data;
 }
 
+//get and show data from api
 const getData = searchText => {
     connectApi(searchText).then(data => {
         if (data.numFound !== 0) {
-            bookSearchArea.textContent = ''
-            totalBooksFound.innerHTML = ''
+            bookSearchArea.textContent = '';
+            totalBooksFound.innerHTML = '';
+            let showBooksCount = 0;
             data.docs.forEach(element => {
-                const singleBook = document.createElement('div')
-                singleBook.classList.add('col')
+                showBooksCount++;
+                const singleBook = document.createElement('div');
+                singleBook.classList.add('col');
                 singleBook.innerHTML = `
                     <div class="card">
-                        <img src="https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg" class="card-img-top" alt="...">
+                        <img src="https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg" class="card-img-top" alt="..." >
                         <div class="card-body">
-                             <h5 class="card-title">${element.title}</h5>
-                            <h6 class="text-muted">by ${element.author_name ? element.author_name : 'Unknow Author'}</h6>
+                             <h4 class="card-title">${element.title}</h4>
+                            <h6 class="text-muted">by ${element.author_name ? element.author_name[0] : 'Unknow Author'}</h6>
                             <p class="card-text"><strong>Publisher:</strong> ${element.publisher ? element.publisher[0] : 'Unknow Publisher'}  | <strong>First published in:</strong> ${element.first_publish_year ? element.first_publish_year : 'Not Found'}
                             </p>
                         </div>
                     </div>                        
-                `
-                bookSearchArea.appendChild(singleBook)
-                //Error Handel Section               
-                errorHandel('hide', 'hide', 'show', 'show')
+                `;
+                bookSearchArea.appendChild(singleBook);
+
             });
             totalBooksFound.innerHTML = `
-                <div class="bg-secondary text-center p-3 text-light fw-bold">
-                    <h2>Total Books Found: ${data.numFound}</h2>
+                <div class="bg-success bg-opacity-75 text-white text-center p-3 fw-bold rounded">
+                    <h4>Total Books Found: ${data.numFound} <span class="text-white">|</span> Books Showing: ${showBooksCount}</h4>
                 </div>
-            `
+            `;
+            //loading and Error function
+            loadingAndError('hide', 'hide', 'hide', 'show');
         }
         else {
-            //Error Handel Section        
-            errorHandel('hide', 'show', 'hide', 'hide')
+            //loading and Error function     
+            loadingAndError('hide', 'hide', 'show', 'hide');
         }
     })
 
 }
 
 
-const errorHandel = (typeAName, noResults, searchArea, totalCounter) => {
+const loadingAndError = (loading, typeAName, noResults, searchArea) => {
+    /* spiner */
+    if (loading === 'show') {
+        lodingData.classList.remove('d-none');
+    }
+    else if (loading === 'hide') {
+        lodingData.classList.add('d-none');
+    }
     /* error typeing */
     if (typeAName === 'show') {
-        erroTypeAName.classList.remove('d-none')
+        erroTypeAName.classList.remove('d-none');
     }
     else if (typeAName === 'hide') {
-        erroTypeAName.classList.add('d-none')
+        erroTypeAName.classList.add('d-none');
     }
+
     /* error no results */
     if (noResults === 'show') {
-        erroNoResults.classList.remove('d-none')
+        erroNoResults.classList.remove('d-none');
     }
     else if (noResults === 'hide') {
-        erroNoResults.classList.add('d-none')
+        erroNoResults.classList.add('d-none');
     }
-    /* error no results */
+
+    /* results area*/
     if (searchArea === 'show') {
-        bookSearchArea.classList.remove('d-none')
+        bookSearchArea.classList.remove('d-none');
+        totalBooksFound.classList.remove('d-none');
     }
     else if (searchArea === 'hide') {
-        bookSearchArea.classList.add('d-none')
-    }
-    /* error no results */
-    if (totalCounter === 'show') {
-        totalBooksFound.classList.remove('d-none')
-    }
-    else if (totalCounter === 'hide') {
-        totalBooksFound.classList.add('d-none')
+        bookSearchArea.classList.add('d-none');
+        totalBooksFound.classList.add('d-none');
     }
 }
